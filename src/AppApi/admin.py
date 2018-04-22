334520567222e860d3c8b90c72d14a37486fc7d6
anupam691997 <anupamrocking.singh@gmail.com>
2018-04-22 10:58:19 +0530

    done

--- src/AppApi/admin.py
+++ src/AppApi/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from .models import ApiKey,AndroidApp,Banner
 from .models import Country
 
-admin.site.register(ApiKey)
+#admin.site.register(ApiKey)
 admin.site.register(AndroidApp)
 admin.site.register(Banner)
 admin.site.register(Country)
